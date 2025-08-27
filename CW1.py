ebcfdd08f8b8c7cea6c663b898abb1bee0cbc85d
SosaM2007 <maliqsosa709@gmail.com>
2025-08-27 09:48:37 -0500

    Add files via upload

---
+++ CW1.py
@@ -0,0 +1,10 @@
+Stu_name=input("Enter the student Name")
+course1 = int(input("Enter the grade points for course1:"))
+course2 = int(input("Enter the grade points for course2:"))
+course3 = int(input("Enter the grade point for course3:"))
+course4 = int(input("Enter the grade points for course4:"))
+course5 = int(input("Enter the grade points for course5:"))
+
+total = course1+course2+course3+course4+course5
+print = ("The total grade points is: 480:")
+
