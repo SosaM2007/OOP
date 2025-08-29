bd37b131b523237b1d188f7ede899c6121763e30
SosaM2007 <maliqsosa709@gmail.com>
2025-08-29 09:55:01 -0500

    Add files via upload

--- CW1.py
+++ CW1.py
@@ -1,4 +1,4 @@
-Stu_name=input("Enter the student Name")
+Stu_name=input("Enter the student Name:")
 course1 = int(input("Enter the grade points for course1:"))
 course2 = int(input("Enter the grade points for course2:"))
 course3 = int(input("Enter the grade point for course3:"))
@@ -6,5 +6,19 @@ course4 = int(input("Enter the grade points for course4:"))
 course5 = int(input("Enter the grade points for course5:"))
 
 total = course1+course2+course3+course4+course5
-print = ("The total grade points is: 480:")
+print ("The total grade points is: 480:")
+average = total / 5
+print("the average is",average)
+
+if average <100 and average >90:
+    print("Grade A")
+elif average <89 and average >79:
+    print("Grade B")
+elif average <79 and total_percentage >69:
+    print("Grade C")
+elif average <59 and average >70:
+    print("Grade D")
+else:
+    print("Grade F")
+
 
