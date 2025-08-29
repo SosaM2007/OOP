162b8266aa9ba36e6c8f8271102ecc4c96c247e3
SosaM2007 <maliqsosa709@gmail.com>
2025-08-29 09:54:37 -0500

    Add files via upload

---
+++ CW2.py
@@ -0,0 +1,27 @@
+a = input(int("Enter the value for a""))
+b = input(int("Enter the value for b"))
+c = input(int("Enter the value for c"))
+
+if a > b and a > c
+  print("a is the greatest")
+elif b > a and b > c
+ print("b is the greatest")
+ elif c > a and c > b
+print("c is the greatest")
+else:
+print("there is a tie for the greatest variable")
+
+
+number1 = int(input("Enter the first number"))
+number2 = int(input("Enter the second number"))
+operator = input("Enter the operator:" )
+
+if operator == "+":
+    Print("the sum is:", number1+number2)
+elif operator == "-":
+    Print("the difference is:", number1-number2)
+elif operator == "*":
+    Print("the product is:" , number1*number2)
+elif operator == "/":
+    Print("the quotient is:" , number1/number2)
+
